23c8c0244294c502a61e1e2ac7f33d390cca9b7e
dgar449 <43426123+dgar449@users.noreply.github.com>
2020-01-20 16:07:08 +1300

    Tried different upload code
    Didn't work

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,17 +34,18 @@ namespace CsvToVcf.Controllers
         {
             long size = files.Sum(f => f.Length);
 
-            // full path to file in temp location
-            var filePath = Path.GetTempFileName();
-
+            var filePaths = new List<string>();
             foreach (var formFile in files)
             {
                 if (formFile.Length > 0)
                 {
+                    // full path to file in temp location
+                    var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+                    filePaths.Add(filePath);
+
                     using (var stream = new FileStream(filePath, FileMode.Create))
                     {
                         await formFile.CopyToAsync(stream);
-                        System.Diagnostics.Debug.WriteLine("Yo your file uploaded!!");
                     }
                 }
             }
@@ -52,7 +53,8 @@ namespace CsvToVcf.Controllers
             // process uploaded files
             // Don't rely on or trust the FileName property without validation.
 
-            return Ok(new { count = files.Count, size, filePath });
+            return Ok(new { count = files.Count, size, filePaths });
+
         }
 
         public IActionResult Index()
