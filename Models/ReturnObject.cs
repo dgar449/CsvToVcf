5b9f2aed2f5c11bd5d15eb584f85cd58095bd966
SankethJain <35916246+SankethJain@users.noreply.github.com>
2020-01-25 12:39:46 +0530

    Change Request #2

---
+++ Models/ReturnObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsvToVcf.Models
+{
+    public class ReturnObject<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+        public Exception Exception { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
